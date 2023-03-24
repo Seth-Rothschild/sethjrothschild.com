4aab130432092fa72390d14d38801b88144c1832
Seth Rothschild <seth.j.rothschild@gmail.com>
2023-03-24 15:55:39 -0400

    Add title to index (closes #2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <h1>Home</h1>
+</body>
+
+<style>
+    body {
+        font-family: 'Lato';
+    }
+    h1 {
+        font-size: 45pt;
+        line-height: 1.2;
+        font-weight: 300;
+        text-align: center;
+    }
+</style>
