a5fd1b8322ec04bc126bc6d6557977d3ff26bf4e
Seth Rothschild <seth.j.rothschild@gmail.com>
2023-03-24 16:28:28 -0400

    Add content to index page (closes #4)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,56 @@
 <head>
-    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+	<link
+		href="http://fonts.googleapis.com/css?family=Lato:400,700"
+		rel="stylesheet"
+		type="text/css"
+	/>
 </head>
 <body>
-    <h1>Home</h1>
+	<h1>Home</h1>
+	<h2>About</h2>
+	<p>
+		I am a Research and Development Software Engineer in the Dell Technologies Office of the CTO
+		Dojo. We research emerging technologies and build prototypes to serve as proofs of concept. I'm
+		an evangelist of Extreme Programming practices which we follow, so feel free to contact me if
+		you want to learn more!
+	</p>
+	<p>
+		Prior to working at Dell I was a Research Scientist at Feature Labs where I worked on the open
+		source project Featuretools. You can also check my Github page for a nice cross section of my
+		interests, or the programming page on the sidebar.
+	</p>
+	<h2>Mathematics</h2>
+	<p>
+		I studied in algebraic groups over finite fields of positive characteristic. I completed my
+		Ph.D. in 2017 under George McNinch at Tufts University in Medford, MA.
+	</p>
+	<p>
+		My thesis work involved constructing unipotent groups by group extensions, and then finding
+		representations of nice groups which contain those extensions. My math at this point is a little
+		rusty, but If you happen to be interested in representations of unipotent algebraic groups I can
+		likely still point you in the right direction.
+	</p>
+	<h2>Contact</h2>
+	<p>gmail: seth.j.rothschild</p>
+	<p>dell: seth.rothschild</p>
+	<h2>Programming</h2>
+	<p>See my programming page.</p>
+
+	<h2>Curriculum Vitae</h2>
+	<p>PDF Version [Updated March 2023]</p>
+
+	<h2>Thesis</h2>
+	<p>PDF Version [Updated April 31 2017]</p>
 </body>
 
 <style>
-    body {
-        font-family: 'Lato';
-    }
-    h1 {
-        font-size: 45pt;
-        line-height: 1.2;
-        font-weight: 300;
-        text-align: center;
-    }
+	body {
+		font-family: 'Lato';
+	}
+	h1 {
+		font-size: 45pt;
+		line-height: 1.2;
+		font-weight: 300;
+		text-align: center;
+	}
 </style>
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,36 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has Home', async ({ page }) => {
+test('that the index page has headers', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Mathematics' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Contact' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Programming' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Curriculum Vitae' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Thesis' })).toBeVisible();
+});
+test('that each section has some content', async ({ page }) => {
+	await page.goto('/');
+
+	// About
+	await expect(page.getByText('Software Engineer')).toBeVisible();
+	await expect(page.getByText('Research Scientist')).toBeVisible();
+
+	// Mathematics
+	await expect(page.getByText('algebraic groups over finite fields')).toBeVisible();
+	await expect(page.getByText('unipotent groups by group extensions')).toBeVisible();
+
+	// Contact
+	await expect(page.getByText('gmail: seth.j.rothschild', { exact: true })).toBeVisible();
+	await expect(page.getByText('dell: seth.rothschild', { exact: true })).toBeVisible();
+
+	// Programming
+	await expect(page.getByText('See my')).toBeVisible();
+
+	// CV
+	await expect(page.getByText('Updated March 2023')).toBeVisible();
+
+	// Thesis
+	await expect(page.getByText('Updated April 31 2017')).toBeVisible();
 });
